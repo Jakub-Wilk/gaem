248f614eac456fa1e3f75f4ace8f17eeffae2474
Jakub Wilk <wilkjakub64@gmail.com>
2021-09-27 21:54:02 +0200

    Changed an interval loop to an animation frame loop

--- scripts/Game.js
+++ scripts/Game.js
@@ -8,37 +8,26 @@ class Game {
         this.last_time = 0 // store for last time checked for delta_time
     }
 
-    get width() {
-        return this.canvas.width
-    }
-
-    get height() {
-        return this.canvas.height
-    }
-
-    add_object = (object) => {
+    add_object(object) {
         this.objects.push(object)
     }
 
-    process_frame = () => {
-        return new Promise((resolve, reject) => {
-            this.delta_time = (performance.now() - this.last_time) / 1000
-            this.last_time = performance.now()
-            this.physics_tick()
-            this.render_tick()
-            resolve()
-        })
+    process_frame() {
+        this.delta_time = (performance.now() - this.last_time) / 1000
+        this.last_time = performance.now()
+        this.physics_tick()
+        this.render_tick()
     }
 
-    physics_tick = () => {
+    physics_tick() {
         for (const object of this.objects) {
             object.run_physics(this.delta_time, this.canvas)
         }
     }
 
-    render_tick = () => {
+    render_tick() {
         this.context.fillStyle = "white"
-        this.context.rect(0, 0, canvas.width, canvas.height)
+        this.context.rect(0, 0, this.canvas.width, this.canvas.height)
         this.context.fill()
         this.context.fillStyle = "black"
         this.context.font = "20px Montserrat"
@@ -48,9 +37,16 @@ class Game {
         }
     }
 
-    run = () => {
-        setInterval(async () => {
-            await this.process_frame()
+    run() {
+        this.set_stage(1)
+        const object = this
+        requestAnimationFrame(function req_frame() {
+            object.process_frame()
+            requestAnimationFrame(req_frame)
         })
+    }
+
+    set_stage() {
+        
     }
 }
