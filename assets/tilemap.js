0a441810d06631be66ab2af7daec2d78f50017f8
Jakub Wilk <wilkjakub64@gmail.com>
2021-09-29 04:03:37 +0200

    Changed tile names to reflect 3D design

--- assets/tilemap.js
+++ assets/tilemap.js
@@ -2,7 +2,7 @@ const tilemap = {
     "color-to-id": {
         "#FF0000": "start",
         "#FFFF00": "view",
-        "#00FF00": "ground",
-        "#00FFFF": "sky"
+        "#00FF00": "wall",
+        "#00FFFF": "floor"
     }
 }
